4714a4fb5dc981636f306845f79eb236926b94a6
jonathan elbom <jb@jonnybomb.com>
2014-03-13 13:02:25 -0500

    transitions added

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,9 +1,9 @@
-beats = {};
+var beats = beats || {};
 
-beats._where = { orig:'where are you?', options: [ 'At SXSW', 'On 6th St', 'The Convention Center', 'Bar 96', 'Stubbs', 'Karma Lounge' ], value: '' };
-beats._feel = { orig: 'doing what?', options: [ 'Getting Weird', 'Getting Lucky', 'Rocking Out', 'Dancing', 'Partying', 'Getting Jiggy Wit It' ], value: '' };
-beats._with = { orig: 'with whom?', options: [ 'My Homies', 'My Pedicab Driver', 'Myself', 'Austin\'s Finest', 'Your Mom', 'Beautiful People' ], value: '' };
-beats._who = { orig: 'which artist?', options: [ 'BANKS', 'DZ Deathrays', 'Kendrick Lamar', 'Phantogram', 'Talib Kweli', 'Zedd' ], value: '' };
+beats._where = { label:'i\'m', orig:'where are you?', options: [ 'At SXSW', 'On 6th St', 'The Convention Center', 'Bar 96', 'Stubbs', 'Karma Lounge' ], value: '' };
+beats._feel = { label:"& feel like", orig: 'doing what?', options: [ 'Getting Weird', 'Getting Lucky', 'Rocking Out', 'Dancing', 'Partying', 'Getting Jiggy Wit It' ], value: '' };
+beats._with = { label:'with', orig: 'with whom?', options: [ 'My Homies', 'My Pedicab Driver', 'Myself', 'Austin\'s Finest', 'Your Mom', 'Beautiful People' ], value: '' };
+beats._who = { label:'who', orig: 'which artist?', options: [ 'BANKS', 'DZ Deathrays', 'Kendrick Lamar', 'Phantogram', 'Talib Kweli', 'Zedd' ], value: '' };
 beats._inputDefault = "write something";
 
 beats.curOptionId = null;
@@ -59,20 +59,67 @@ beats.addHandlers = function() {
 	});
 };
 
-beats.getText = function ( section ) {
-	var data = beats[ '_' + section ];
-	var str = ( data.value === '' || data.value === data.orig ) ? data.orig.toUpperCase() : data.value.toUpperCase();
-	return encodeURIComponent( str );
-};
-
 beats.transition = function( screen ) {
+	var delay = 50;
+	var flipTime = 500;
+	var duration = 200
 	if ( screen === 'main' ) {
-		beats.$main.find('div.action').each( function( index, elem ) {
-			$(elem).removeClass( 'hidden' );
+		beats.$main.find('.trans').each( function(index, elem) {
+			var $elem = $(elem);
+			switch ( elem.tagName.toLowerCase() ) {
+			case 'div':
+				$elem.attr('data-time', index * delay + flipTime);
+				$elem.find('.bg.wrapper').css('width', '90%');
+				$elem.hide().delay( index * delay ).show( {duration:0, start:beats.startMainElemIntro} );
+				break;
+			}			
 		});
 	}
 	else if  ( screen === 'author' ) {
+		beats.$author.find('.trans').each( function(index, elem) {
+			var $elem = $(elem);
+			switch ( elem.tagName.toLowerCase() ) {
+			case 'div':
+				//$elem.attr('data-time', index * delay + flipTime);
+				//$elem.find('.bg.wrapper').css('width', 'auto');
+				$elem.hide().delay( index * delay ).show( {duration:0, start:beats.startAuthorElemIntro} );
+				break;
+			}			
+		});
+	}
+};
 
+beats.startAuthorElemIntro = function( e ) {
+	//console.log('startAuthorElemIntro, this.tagName:',this.tagName);
+	if ( this.tagName.toLowerCase() === 'div' ) {
+		var $this = $(this);
+		var $option = $this.find('.option');
+		$this.find('.bg').addClass('flip');
+		//console.log('this:',this);
+		//console.log('$option:',$option);
+		return;
+
+		if ( $option.length > 0 ) {
+			console.log('$option.width():',$option.outerWidth() );
+			$this.find('.bg.wrapper').animate( { width:$option.outerWidth() + 20 }, 1000)
+		}
+		
+	}
+};
+
+beats.startMainElemIntro = function( e ) {
+	//console.log('startMainElemIntro, this.tagName:',this.tagName);
+	if ( this.tagName.toLowerCase() === 'div' ) {
+		var $this = $(this);
+		var $action = $this.find('.action');
+		$this.find('.bg').addClass('flip');
+		//console.log('this:',this);
+		//console.log('$action:',$action);
+		if ( $action.length > 0 ) {
+			//console.log('$action.width():',$action.outerWidth() );
+			$this.find('.bg.wrapper').animate( { width:$action.outerWidth() }, 500)
+		}
+		
 	}
 };
 
@@ -103,7 +150,10 @@ beats.showAuthoringScreen = function ( elem ) {
 	var $icon = beats.$author.find('i');
 
 	beats.curOptionId = $elem.attr('id');
-	beats.$author.find('span.static').text( $elem.prev().text() );
+	beats.$author.find('span.static').text( beats[ '_' + beats.curOptionId ].label );
+	
+	console.log('$elem:',$elem);
+
 	$input.val( beats._inputDefault );
 
 	var options = beats[ '_' + beats.curOptionId ].options;
@@ -131,11 +181,13 @@ beats.showScreen = function ( screen ) {
 		beats.$main.removeClass( 'hidden' );
 		beats.$tweet.removeClass( 'hidden' );
 		beats.$author.addClass( 'hidden' );
+		beats.transition( 'main' );
 		break;
 	case 'author':
 		beats.$main.addClass( 'hidden' );
 		beats.$tweet.addClass( 'hidden' );
 		beats.$author.removeClass( 'hidden' );
+		beats.transition( 'author' );
 		break;
 	}
 }
@@ -147,6 +199,6 @@ $(document).ready(function() {
 	beats.$tweet = $('div.tweet');
 
 	beats.addHandlers();
-	beats.transition( 'main' );
+	//beats.transition( 'main' );
 	beats.showMainScreen();
 });
